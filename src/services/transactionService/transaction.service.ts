c5a539d603bd8742998710dedb608f62bbdabdc6
khairulazman67 <azman.khairul67@gmail.com>
2025-08-04 00:10:50 +0700

    merge

--- src/services/transactionService/transaction.service.ts
+++ src/services/transactionService/transaction.service.ts
@@ -35,11 +35,11 @@ export class TransactionService implements ITransactionService {
         " pd0_" +
         data.headerPrint.row1 +
         " pd1_" +
-         data.headerPrint.row2 + 
+        data.headerPrint.row2 +
         " pd2_" +
         data.headerPrint.row3 +
         " pd3_";
-        data.headerPrint.row4;
+      data.headerPrint.row4;
     }
 
     await prisma.$transaction(async (tx) => {
@@ -81,8 +81,8 @@ export class TransactionService implements ITransactionService {
         await this.etalaseRepository.update(dataEtalase.id, etalaseSave, tx);
 
         console.log("dataEtalase ", dataEtalase);
-<<<<<<< HEAD
-        for (let i = 0; i < 0; i++) {
+
+        for (let i = 0; i < item.amount; i++) {
           payloadVM =
             payloadVM +
             " pn_" +
@@ -92,25 +92,10 @@ export class TransactionService implements ITransactionService {
             item.usageRules +
             " pz";
         }
-=======
-
-
-        for( let i =0; i<item.amount;i++){
-          payloadVM =
-          payloadVM +
-          " pn_" +
-          dataEtalase.displayCode +
-          dataEtalase.medicineName +
-          " " +
-          item.usageRules+' pz'
-        }
-         
->>>>>>> f387dbb6863d48f4dd4641e8df4a1bd95a358767
       }
     });
 
-
-     await this.vendingMachineIntegration.sendRequest(payloadVM);
+    await this.vendingMachineIntegration.sendRequest(payloadVM);
 
     return payloadVM;
   }
